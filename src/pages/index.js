71c779fcb5411794cf64e9b62ab81076d507183c
Tharshen <tharshensurianbalan@gmail.com>
2023-03-13 13:04:30 +0800

    fixed routing

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@component/styles/Home.module.css'
-import Fetchingdata from './FetchingData'
+import Fetchingdata from '@component/components/FetchingData'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
